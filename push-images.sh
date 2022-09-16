3669a001a9058b1c1610e490523f078e46bb11fd
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-09-16 12:54:43 -0400

    Publish v2022.09.1. [release] (#3)

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/aws:2022.06
-docker push cimg/aws:2022.06.1
+docker push cimg/aws:2022.09
+docker push cimg/aws:2022.09.1
