344faeae5827ac9db0cbb3967a70515ceacd07ed
Jeff Chen <jeff.chen@circleci.com>
2023-01-23 18:28:24 -0800

    Publish v2023.01.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2022.11/Dockerfile -t cimg/aws:2022.11.1 -t cimg/aws:2022.11 .
+docker build --file 2023.01/Dockerfile -t cimg/aws:2023.01.1 -t cimg/aws:2023.01 .
