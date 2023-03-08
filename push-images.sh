7fc0a08bd13b0effc8506456daf35de70f8b1341
Adam Hartley <BytesGuy@users.noreply.github.com>
2023-03-08 21:00:01 +0000

    Publish v2023.03.1. [release] (#11)

--- push-images.sh
+++ push-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/aws:2023.01
-docker push cimg/aws:2023.01.1
+docker push cimg/aws:2023.03
+docker push cimg/aws:2023.03.1
