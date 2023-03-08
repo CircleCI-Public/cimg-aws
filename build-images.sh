a8e9efb69c751b5f7a2131073bfb0cbcabf17a09
Adam Hartley <git@ahartley.com>
2023-03-08 16:22:57 +0000

    Publish v2023.03.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 2023.01/Dockerfile -t cimg/aws:2023.01.1 -t cimg/aws:2023.01 .
+docker build --file 2023.03/Dockerfile -t cimg/aws:2023.03.1 -t cimg/aws:2023.03 .
