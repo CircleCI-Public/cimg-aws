86bfd584baeb592ef67438963bff3204d90d951f
Jalexchen <jeff.chen@circleci.com>
2023-09-25 14:08:24 -0700

    feat: add awsFeed script (#22)

---
+++ awsFeed.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ -f shared/automated-updates.sh ]; then
+  source shared/automated-updates.sh
+else
+  echo "Check if submodule was loaded; automated-updates.sh is missing"
+  exit 1
+fi
+source ./manifest
+
+templateFile="Dockerfile.template"
+interval="monthly"
+
+function awsFeed() {
+  replaceDatedTags "$templateFile" "$interval"
+  if docker buildx imagetools inspect "$namespace/$parent:$RELEASE-node" &> /dev/null; then
+    [[ -n $STRING_TO_REPLACE || -n $RELEASEMONTHLY ]] && ./shared/release "$RELEASEMONTHLY" || exit 0
+  else
+    echo "cimg/deploy:$RELEASE-node does not exist"
+    exit 0
+  fi
+}
+
+awsFeed "$templateFile"
